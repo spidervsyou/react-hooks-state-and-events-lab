7d75260d96adbb23db5aa3a86996c28800c796b4
SPIDER <162958422+spidervsyou@users.noreply.github.com>
2024-05-09 20:36:47 +0300

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from 'react';
 
-function Item({ name, category }) {
+function Item({ item }) {
+  const [inCart, setInCart] = useState(false);
+
+  const toggleCart = () => {
+    setInCart(prevInCart => !prevInCart);
+  };
+
   return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+    <li className={inCart ? 'in-cart' : ''}>
+      {item.name}
+      <button onClick={toggleCart}>
+        {inCart ? 'Remove From Cart' : 'Add to Cart'}
+      </button>
     </li>
   );
 }
